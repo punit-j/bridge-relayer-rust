f0ab04d448132d6a76e60c9d169ae7d987bbfc13
Misha Brazhnikov <br.misha@gmail.com>
2022-03-16 13:21:20 +0200

    added the "v1/health"

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use rocket::State;
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+struct HitCount {
+    count: AtomicUsize
+}
+
+#[get("/health")]
+fn health() -> String {
+    "OK".to_string()
+}
+
+#[rocket::main]
+async fn main() {
+    let mut rr = rocket::build();
+    rr = rr.mount("/v1", routes![health]);
+    rr.launch().await;
 }
