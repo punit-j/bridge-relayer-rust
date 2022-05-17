cf4d2c8b4cda68e31718017c2674224f6c9b5c0f
Misha <br.misha@gmail.com>
2022-05-17 11:26:44 +0300

    Receiving event done

--- src/near/mod.rs
+++ src/near/mod.rs
@@ -1,5 +1,3 @@
-//pub mod near_event;
-
 use std::io::Write;
 use near_lake_framework::{LakeConfig, LakeConfigBuilder};
 use near_lake_framework::near_indexer_primitives::types::{AccountId, BlockHeight};
@@ -8,10 +6,6 @@ use near_lake_framework::near_indexer_primitives::views::{
 };
 use std::str::FromStr;
 use transfer_event_custon_logs as cust_events;
-
-use near_sdk::{log, serde_json, serde::Serialize, serde::Deserialize};
-
-//use my_librrr;
 
 pub async fn run_watcher() {
     let config = LakeConfigBuilder::default()
@@ -24,46 +18,22 @@ pub async fn run_watcher() {
     let mut stream = near_lake_framework::streamer(config);
 
     while let Some(streamer_message) = stream.recv().await {
-        let block_str = serde_json::to_string(&streamer_message).unwrap();
-       /* if streamer_message.block.header.height != 90027479 {
-            continue;
-        }
-
-        println!("ms {}", sss);*/
-       // continue;
-        if block_str.contains("nep297") {
-            println!("ms {}", block_str);
-        }
-
         //println!("\r\nBlock {} {}", streamer_message.block.header.height, streamer_message.block.author);
         for shard in streamer_message.shards {
-            /*let sss = serde_json::to_string(&shard).unwrap();
-            if !sss.contains("AVsQPZGamsnkeaZ3bkFaU8iaQYSSJB5zG8Kf4jcf7i9A") {
-                continue;
-            }
-            println!("ms {}", sss);
-
-            if let Some(chunk) = shard.chunk {
-
-            }*/
-
             for outcome in shard.receipt_execution_outcomes {
                 let contract_name =  AccountId::from_str("dweth_beta.nearlend.testnet").unwrap();
 
-                if /*contract_name == outcome.receipt.receiver_id*/1==1 {
+                if contract_name == outcome.receipt.receiver_id {
                     //println!("{} predecessor_id: {:?}, receiver_id: {:?}", streamer_message.block.header.height, outcome.receipt.predecessor_id, outcome.receipt.receiver_id);
 
                     for log in outcome.execution_outcome.outcome.logs {
-                           let EVENT_JSON: &'static str = "EVENT_JSON:";
+                        let EVENT_JSON: &'static str = "EVENT_JSON:";
                         if log.starts_with(EVENT_JSON) {
                             let r = parce_event_json(&log.as_str()[EVENT_JSON.len()..]);
-                            if block_str.contains("nep297") {
-                                println!("l {}", &log.as_str()[EVENT_JSON.len()..]);
-                            }
-
                             match r {
                                 Ok(r) => {
-                                    println!("Event: {}, data: {}", r.event, r.data[0]);
+                                    println!("Event: {}, data: {}", r.event, r.data);
+                                    // TODO:
                                 }
                                 Err(e) => {
                                     if !matches!(e, ParceError::NotEvent){
@@ -99,15 +69,6 @@ fn parce_event_message(json: &str) -> serde_json::Result<cust_events::EventMessa
     let r: serde_json::Result<cust_events::EventMessage> = serde_json::from_str(json);
     r
 }
-/*
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(crate = "near_sdk::serde")]
-pub struct EventMessage {
-    pub standard: String,
-    pub version: String,
-    pub event: serde_json::Value,
-    pub data: serde_json::Value
-}*/
 
 fn parce_event_json(json: &str) -> Result<cust_events::EventMessage, ParceError> {
     let r: serde_json::Result<cust_events::EventMessage> = serde_json::from_str(json);
