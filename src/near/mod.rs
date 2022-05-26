4a9bf85786e2b0e5b9399dbfc558488b793e88a9
Misha <br.misha@gmail.com>
2022-05-26 19:10:09 +0300

    Removed comments and add the contract_name and arg of the run_watcher

--- src/near/mod.rs
+++ src/near/mod.rs
@@ -10,24 +10,19 @@ use rocket::form::validate::Len;
 use spectre_bridge_common::Event;
 use serde_json::json;
 
-pub async fn run_watcher() {
+pub async fn run_watcher<'a>(contract_name: &'a AccountId) {
     let config = LakeConfigBuilder::default()
         .testnet()
-        .start_block_height(89175285)
+        .start_block_height(90587572)
         .build()
         .expect("Failed to build LakeConfig");
-    //let x = spectre - bridge - protocol::near::contracts::transfer::src::;
-    // instantiate the NEAR Lake Framework Stream
+
     let mut stream = near_lake_framework::streamer(config);
 
     while let Some(streamer_message) = stream.recv().await {
-        //println!("\r\nBlock {} {}", streamer_message.block.header.height, streamer_message.block.author);
         for shard in streamer_message.shards {
             for outcome in shard.receipt_execution_outcomes {
-                let contract_name =  AccountId::from_str("transfer.spectrebridge2.testnet").unwrap();
-
-                if contract_name == outcome.receipt.receiver_id {
-                    //println!("{} predecessor_id: {:?}, receiver_id: {:?}", streamer_message.block.header.height, outcome.receipt.predecessor_id, outcome.receipt.receiver_id);
+                if *contract_name == outcome.receipt.receiver_id {
                     for log in outcome.execution_outcome.outcome.logs {
                         if let Some(json) = spectre_bridge_common::remove_prefix(log.as_str()) {
                             match get_event(json) {
@@ -42,17 +37,12 @@ pub async fn run_watcher() {
                                 }
                             }
                         }
-                        //println!("log: {}", log);
                     }
                 }
 
             }
         }
     }
-}
-
-pub enum Error {
-
 }
 
 pub struct JsonError(pub serde_json::Error);
@@ -82,7 +72,7 @@ pub fn fix_json(mut json: serde_json::Value) -> serde_json::Value {
 pub fn get_event(mut json: serde_json::Value) -> Result<spectre_bridge_common::Event, ParceError> {
     let mut json = fix_json(json);
 
-    let r = serde_json::from_value::<spectre_bridge_common::EventMessage>(json.clone());  // TODO: try to remove "clone"
+    let r = serde_json::from_value::<spectre_bridge_common::EventMessage>(json.clone());
     let r = r.map_err(|e| ParceError::Json(e))?;
 
     if r.standard != spectre_bridge_common::STANDARD {
