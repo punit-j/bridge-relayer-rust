fbbb7b428b0b3c78474ef4251c800fb75abaca1b
Misha <br.misha@gmail.com>
2022-05-28 14:50:08 +0300

    Implemmented the get_proof

--- src/ethereum/proof.rs
+++ src/ethereum/proof.rs
@@ -1,14 +1,44 @@
+
+use std::process;
+use serde_json::json;
 use web3::contract::{Contract, Options};
 use web3::ethabi::ParamType::String;
 use web3::ethabi::Uint;
 use web3::types::{H256, TransactionReceipt, U256};
 use web3::Web3;
+use spectre_bridge_common;
+use web3::api::Eth;
 
 #[derive(Debug)]
 pub enum Error<'a> {
     Empty,
     Other(&'a str),
-    Web3(web3::Error)
+    Web3(web3::Error),
+    Json(serde_json::Error),
+}
+
+pub async fn get_proof<T: web3::Transport>(url: std::string::String,
+                                           client: &web3::api::Eth<T>,
+                                           index_js: std::string::String,
+                                           tr_hash: H256)
+    -> Result<spectre_bridge_common::Proof, Error<'_>> {
+    let log_index = get_transaction_log_index(&client, tr_hash).await?;
+
+    let json_args = json!({"logIndex": log_index.as_u64(), "transactionHash": tr_hash});
+
+    let mut command = process::Command::new("node");
+    command.arg(index_js).arg("eth-to-near-find-proof")
+        .arg(json_args.to_string())
+        .arg("--eth-node-url").arg(url);
+
+    let rr = command.output().map_err(|e| Error::Other("Unable to unwrap output"))?.stdout;
+    let mut out = std::str::from_utf8(&rr).map_err(|e| Error::Other("Unable to parse output"))?;
+
+    let json = serde_json::from_str::<serde_json::Value>(out).map_err(|e| Error::Json(e))?;
+    let json= json.get("proof_locker").ok_or(Error::Other("JSON doesnt contain the proof_locker"))?;
+
+    let res = serde_json::from_value::<spectre_bridge_common::Proof>(json.clone()).map_err(|e| Error::Json(e))?;
+    Ok(res)
 }
 
 pub async fn get_transaction_log_index<T: web3::Transport>(client: &web3::api::Eth<T>, tr_hash: H256) -> Result<U256, Error<'_>> {
